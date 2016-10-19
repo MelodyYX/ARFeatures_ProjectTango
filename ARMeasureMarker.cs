af9a437a6e592f51396361ef3c3ca631328b7d48
Xiang,Yue <yxi41@uclive.ac.nz>
2016-10-19 17:28:29 +1300

    first commit

---
+++ ARMeasureMarker.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ARMeasureMarker : MonoBehaviour {
+
+	public int m_type = 0;
+
+	public float m_timestamp = -1.0f;
+
+	public Matrix4x4 m_deviceTMarker = new Matrix4x4();
+
+    /// <summary>
+    /// The animation playing.
+    /// </summary>
+    private Animation m_anim;
+
+    public void Start()
+    {
+        m_anim = GetComponent<Animation>();
+        m_anim.Play("Show", PlayMode.StopAll);
+    }
+
+    public void Hide()
+    {
+        m_anim.Play("Hide", PlayMode.StopAll);
+    }
+
+    public void HideDone()
+    {
+        Destroy(gameObject);
+    }
+}
